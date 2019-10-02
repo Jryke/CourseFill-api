4187a354c484bec8008e5c4d11c74ef37e82be52
MrNivorous <karn.sood@gmail.com>
2019-10-02 16:38:38 +0700

    updated info to populate teachers

--- controllers/getCourse.js
+++ controllers/getCourse.js
@@ -1,7 +1,7 @@
 let Courses = require('../models/courses.js')
 
 module.exports = (req, res) => {
-Courses.findById(req.params.id)
+Courses.findById(req.params.id).populate('teachers students')
  .then(data => {res.send(data)})
  .catch(err => {
  res.send(err)
