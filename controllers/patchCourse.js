17e509f2153a04619807df2cc397680f5661af61
MrNivorous <karn.sood@gmail.com>
2019-10-04 10:13:41 +0700

    Update patchCourse to work

--- controllers/patchCourse.js
+++ controllers/patchCourse.js
@@ -1,9 +1,8 @@
 const Courses = require('../models/courses.js')
 
-
 let patchCourse =
 (req, res) => {
-	Courses.update(req.body).then(data => res.send(data))
+	Courses.findByIdAndUpdate(req.params.id, req.body, {new: true}).then(data => res.send(data))
 	.catch(err => {console.log(err)})
 }
 
