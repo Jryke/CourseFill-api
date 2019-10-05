5fb4bb69ebed1741ad5c5874d1889dfa4a5ceff9
MrNivorous <karn.sood@gmail.com>
2019-10-05 15:41:37 +0700

    populate teachers and courses for individual students

--- controllers/getUser.js
+++ controllers/getUser.js
@@ -1,7 +1,14 @@
 const User = require('../models/user')
 
 module.exports = (req, res) => {
-User.findById(req.params.id)
+User.findById(req.params.id).populate({
+	path: 'courses',
+	model: 'courses',
+	select: 'schedule teachers subject name',
+	populate: {
+		path: 'teachers',
+		model: 'user',
+	 	select: 'first_name last_name avatar'}})
  .then(data => {res.send(data)})
  .catch(err => {
  res.send(err)
