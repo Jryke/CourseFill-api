0b9bf87338bbda50298ab1ae110afd68416bf3fe
MrNivorous <karn.sood@gmail.com>
2019-10-05 13:49:06 +0700

    cleaned up getStudents route

--- controllers/getStudents.js
+++ controllers/getStudents.js
@@ -6,11 +6,11 @@ module.exports = (req, res) => {
 Users.find({role: 'student'}).populate({
 	path: 'courses',
 	model: 'courses',
+	select: 'schedule teachers subject name',
 	populate: {
 		path: 'teachers',
 		model: 'user',
 	 	select: 'first_name last_name avatar'}})
-
 .then(data => {res.send(data)})
  .catch(err => {
  res.send(err)
