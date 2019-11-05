a72a5656e5c552914e0903ea8b7a021acc83d659
Jryke <rykerson@gmail.com>
2019-11-05 16:26:41 -0500

    logs removed

--- controllers/postAuthorize.js
+++ controllers/postAuthorize.js
@@ -3,9 +3,7 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res) => {
   let token = req.headers.authorization.replace('Bearer ', '')
-  console.log('token', token)
   let userInfo = jwt.verify(token, process.env.TOKEN_SECRET)
-  console.log('userInfo', userInfo)
   User.findById(userInfo._id).select('-__v')
   .then(user => res.send(user))
   .catch(err => console.log(err))
